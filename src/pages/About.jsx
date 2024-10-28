cfd4058635ead4b03ecdc59cbe45c04a74ee6696
bibiamina <ahnafnihal0999@gmail.com>
2024-10-28 21:02:38 +0600

    about page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='container mx-auto w-auto border rounded-sm bg-gray-500'>
- <p className='font-serif '>
+    <div className='container mx-auto h-96 w-auto border bg-gray-500/25 flex items-center justify-center'>
+ <p className='font-serif text-black ps-10 pe-10 w-[80%] h-[50%] '>
   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia officiis distinctio vitae asperiores blanditiis exercitationem error saepe hic quo dolores! Dolores corrupti dicta ab sint laboriosam. Veniam sit reprehenderit culpa explicabo distinctio itaque temporibus omnis beatae impedit quia! Pariatur eaque aliquam cumque, eius facilis, libero facere aperiam excepturi sunt eum tempore, debitis distinctio magni perspiciatis quidem assumenda laborum alias dicta cupiditate! Officiis quae praesentium impedit perspiciatis omnis earum voluptas cupiditate eum totam, voluptatem optio quam, vel at laborum, minima voluptatibus velit dolore. Error ipsam aut vitae doloribus hic, voluptatum cumque excepturi, expedita odio, impedit quaerat voluptatem. Qui harum iste nisi!
  </p>
+   <img src="aboutpic.jpg" alt="error"  className='h-full shadow-lg object-cover object-center'/>
       
     </div>
   )
